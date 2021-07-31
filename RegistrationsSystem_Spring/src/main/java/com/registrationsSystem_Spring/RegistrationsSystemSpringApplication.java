2a1878db544c93d4c07a69b961188ef051b04444
Mansi Wakharkar <mansiswakharkar@gmail.com>
2021-07-31 22:33:57 +0300

    Initial commit for back end system for Registration System

---
+++ RegistrationsSystem_Spring/src/main/java/com/registrationsSystem_Spring/RegistrationsSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.registrationsSystem_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationsSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationsSystemSpringApplication.class, args);
+	}
+
+}
